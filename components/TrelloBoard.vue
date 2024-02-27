d525e91b345ff3151420ee44e8194a3405f8fffa
Mohammed Taqi <taqealibade@gmail.com>
2024-02-27 22:33:35 +0300

    adding prettier

--- components/TrelloBoard.vue
+++ components/TrelloBoard.vue
@@ -1,75 +1,86 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref } from 'vue'
 import type { Column } from '../types'
-import draggableComponent from 'vuedraggable';
-import { nanoid } from 'nanoid';
+import draggableComponent from 'vuedraggable'
+import { nanoid } from 'nanoid'
 const columns = ref<Column[]>([
-    {
+  {
+    id: nanoid(),
+    title: 'Backlog',
+    tasks: [
+      {
         id: nanoid(),
-        title: "Backlog",
-        tasks: [
-            {
-                id: nanoid(),
-                title: "Create marketing landing page",
-                createdAt: new Date()
-            },
-            {
-                id: nanoid(),
-                title: "Develop cool new feature",
-                createdAt: new Date()
-            },
-            {
-                id: nanoid(),
-                title: "Fix page nav bug",
-                createdAt: new Date()
-            }
-        ],
-    },
-    {
+        title: 'Create marketing landing page',
+        createdAt: new Date(),
+      },
+      {
         id: nanoid(),
-        title: "Selected for Dev",
-        tasks: [],
-    },
-    {
+        title: 'Develop cool new feature',
+        createdAt: new Date(),
+      },
+      {
         id: nanoid(),
-        title: "In Progress",
-        tasks: [],
-    },
-    {
-        id: nanoid(),
-        title: "QA",
-        tasks: [],
-    },
-    {
-        id: nanoid(),
-        title: "Complete",
-        tasks: [],
-    }
-]);
+        title: 'Fix page nav bug',
+        createdAt: new Date(),
+      },
+    ],
+  },
+  {
+    id: nanoid(),
+    title: 'Selected for Dev',
+    tasks: [],
+  },
+  {
+    id: nanoid(),
+    title: 'In Progress',
+    tasks: [],
+  },
+  {
+    id: nanoid(),
+    title: 'QA',
+    tasks: [],
+  },
+  {
+    id: nanoid(),
+    title: 'Complete',
+    tasks: [],
+  },
+])
 </script>
 <template>
-    <div>
-        <draggableComponent 
-        v-model="columns" 
-        group="columns" 
-        :animation="150"
-        handle=".drag-handle"
-        item-key="id" 
-        class=" flex gap-4 overflow-x-auto  items-start"
-        >
-            <template #item="{ element: column }: { element: Column }">
-                <div class="column bg-gray-200 p-5 rounded min-w-[250px]">
-                    <header class=" font-bold mb-4">
-                        <DragHandle />
-                        {{ column.title }}
-                    </header>
-                    <TrelloBoardTask v-for="task in column.tasks" :task="task" :key="task.id" />
-                    <footer>
-                        <button class="text-gray-500">+ Add a Card</button>
-                    </footer>
-                </div>
-            </template>
-        </draggableComponent>
-
-    </div>
+  <div>
+    <draggableComponent
+      v-model="columns"
+      group="columns"
+      :animation="150"
+      handle=".drag-handle"
+      item-key="id"
+      class="flex gap-4 overflow-x-auto items-start"
+    >
+      <template #item="{ element: column }: { element: Column }">
+        <div class="column bg-gray-200 p-5 rounded min-w-[250px]">
+          <header class="font-bold mb-4">
+            <DragHandle />
+            {{ column.title }}
+          </header>
+          <draggableComponent
+            v-model="columns"
+            group="columns"
+            :animation="150"
+            handle=".drag-handle"
+            item-key="id"
+            class="flex gap-4 overflow-x-auto items-start"
+          ></draggableComponent>
+          <TrelloBoardTask
+            v-for="task in column.tasks"
+            :task="task"
+            :key="task.id"
+          />
+          <footer>
+            <button class="text-gray-500">+ Add a Card</button>
+          </footer>
+        </div>
+      </template>
+    </draggableComponent>
+  </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,25 @@
-//https://nuxt.com/docs/api/configuration/nuxt-config
+// https://nuxt.com/docs/api/configuration/nuxt-config
+// eslint-disable-next-line no-undef
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules:["@nuxtjs/tailwindcss", "@vueuse/nuxt"],
+  modules: ['@nuxtjs/tailwindcss', '@vueuse/nuxt', '@nuxtjs/eslint-module'],
+  // eslint: {
+  //   // Enable Prettier integration
+  //   formatter: 'prettier'
+  //   // Optionally, you can specify the Prettier options
+  //   // prettier: {
+  //   //   semi: false,
+  //   //   singleQuote: true,
+  //   //   tabWidth: 2,
+  //   //   useTabs: false
+  //   // }
+  // },
   ssr: false,
   css: ['~/public/main.css'],
   postcss: {
     plugins: {
       tailwindcss: {},
-      autoprefixer: {},
-    },
-  },
+      autoprefixer: {}
+    }
+  }
 })
