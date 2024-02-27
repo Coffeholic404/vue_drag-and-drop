b2830c0f6fab44d5b3ca9af46e635ada1602adec
Mohammed Taqi <taqealibade@gmail.com>
2024-02-27 19:49:57 +0300

    Create a Task Component and Style it with Tailwind CSS

--- components/TrelloBoard.vue
+++ components/TrelloBoard.vue
@@ -47,16 +47,21 @@ const columns = ref<Column[]>([
 ]);
 </script>
 <template>
-    <div class=" flex gap-4 overflow-x-auto items-start">
+    <div class=" flex gap-4 overflow-x-auto  items-start">
         <div v-for="column in columns" :key="column.id"
         class="column bg-gray-200 p-5 rounded min-w-[250px]"
         >
-        <header>
+        <header class=" font-bold mb-4">
             {{ column.title }}
         </header>
-        <p v-for="task in column.tasks" :key="column.id">
-        {{ task.title }}
-        </p>
+        <TrelloBoardTask 
+        v-for="task in column.tasks" 
+        :task="task"
+        :key="task.id"
+        />
+        <footer>
+            <button class="text-gray-500">+ Add a Card</button>
+        </footer>
         </div>
     </div>
 </template>
