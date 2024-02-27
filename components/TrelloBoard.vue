6a5a39be7fc605beec0f141709ea3b463d994f3e
Mohammed Taqi <taqealibade@gmail.com>
2024-02-27 20:34:40 +0300

    adding eslint standard presets

--- components/TrelloBoard.vue
+++ components/TrelloBoard.vue
@@ -49,7 +49,12 @@ const columns = ref<Column[]>([
 </script>
 <template>
     <div>
-        <draggableComponent v-model="columns" group="columns" item-key="id" class=" flex gap-4 overflow-x-auto  items-start">
+        <draggableComponent 
+        v-model="columns" 
+        group="columns" 
+        item-key="id" 
+        class=" flex gap-4 overflow-x-auto  items-start"
+        >
             <template #item="{ element: column }: { element: Column }">
                 <div class="column bg-gray-200 p-5 rounded min-w-[250px]">
                     <header class=" font-bold mb-4">
