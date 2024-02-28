78b288964d9c760e2df82ff6d3f939aa9e8c0b61
Mohammed Taqi <taqealibade@gmail.com>
2024-02-28 15:55:30 +0300

    Implementing Delete Tasks with Backspace

--- components/TrelloBoardTask.vue
+++ components/TrelloBoardTask.vue
@@ -1,14 +1,30 @@
+<!-- eslint-disable func-call-spacing -->
+<!-- eslint-disable no-undef -->
 <script setup lang="ts">
-import type { Task } from '~/types'
-defineProps<{
+import type { Task, ID } from '~/types'
+const props = defineProps<{
   task: Task
 }>()
+
+const emit = defineEmits<{
+  (e: 'delete', payload: ID): void
+}>()
+
+const focused = ref(false)
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+onKeyStroke('Backspace', (e) => {
+  if (focused.value) emit('delete', props.task.id)
+})
 </script>
 
 <template>
   <div
     class="task bg-white p-2 mb-2 rounded shadow-sm max-w-[250px] flex"
     :title="task.createdAt.toLocaleDateString()"
+    tabindex="0"
+    @focus="focused = true"
+    @blur="focused = false"
   >
     <DragHandle class="pr-2" />
     <span>
@@ -29,5 +45,11 @@ defineProps<{
 .sortable-ghost .task::after {
   content: '';
   @apply absolute top-0 bottom-0 left-0 right-0 bg-slate-300 rounded;
+}
+
+.task:focus,
+.task:focus-visible {
+  @apply outline-gray-400 !important;
+  outline: gray auto 1px;
 }
 </style>
